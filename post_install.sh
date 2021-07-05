f97fe437eb575507ccd12dd2b54d55749c67bc49
Fletcher Nichol <fnichol@nichol.ca>
2021-07-05 16:38:31 -0600

    refactor: update `post_install.sh` to use functions
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
-set -eu
+
+main() {
+  set -eu
+  if [ -n "${DEBUG:-}" ]; then
+    set -x
+  fi
+
+  configure
+  enable_services
+  start_services
+}
+
+configure() {
+  true
+}
+
+enable_services() {
+  true
+}
+
+start_services() {
+  true
+}
 
 log() {
   echo "$1" >>/root/PLUGIN_INFO
 }
+
+main "$@"
